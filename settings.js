0d2963f33d294174d84481cd7ee385c55323d797
K29promax <154527751+DELMAX29@users.noreply.github.com>
2024-09-05 12:37:18 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '94701992015' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=nMkQkQgQ#N7JypaI2SdTR2z56QP6m5L97rqUwN4fD5FMDI7-KEf0' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=KAFCRSwZ#Z-8j9F6CptYINcrGNhHr9fgA5rDn5-6MHDuttIOROL8' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
